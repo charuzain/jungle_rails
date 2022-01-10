c227ff60e469bb19ad605f2a69048582ec761447
charuzain <charuabhizain@gmail.com>
2022-01-10 11:44:42 -0500

    user authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,20 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+
   resources :about, only: [:index]
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-        resources :categories, only: [:index, :new, :create]
-
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
